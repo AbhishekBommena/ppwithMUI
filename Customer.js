7ee5913f0f4b1e45dd8143db9f3a89e56a8d30fe
ABHISHEK BOMMENA <67004365+AbhishekBommena@users.noreply.github.com>
2022-08-09 19:32:18 +0000

    Add files via upload

--- Customer.js
+++ Customer.js
@@ -1,13 +1,23 @@
+import { MenuItem } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import { useState } from 'react';
+import CurrencyRupeeIcon  from '@mui/icons-material/CurrencyRupee';
+import CurrencyPoundIcon from "@mui/icons-material/CurrencyPound" 
+import CurrencyYenIcon from "@mui/icons-material/CurrencyYen"
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney"
+import EuroIcon from "@mui/icons-material/Euro" 
+// import {CurrencyRupeeIcon, CurrencyPoundIcon, CurrencyYenIcon, AttachMoneyIcon, EuroIcon } from '@mui/icons-material';
 import "../css/Form.css"
 function Customer(props) {
-
+    const [currency,setCurrency] = useState("")
     const custDetails = props.custDetails
+    const handleCurrency = (event) => {
+        setCurrency(event.target.value)
+    }
     return (
         <>
-            <Grid xs={12} md={6} className="grid-item">
+            <Grid item  xs={12} md={6} className="grid-item">
                 <TextField id="filled-read-only-input"
                     InputProps={{
                         readOnly: true,
@@ -17,7 +27,7 @@ function Customer(props) {
                     className="inputField"
                     value={custDetails?.acctHolderName ?? ""} />
             </Grid>
-            <Grid xs={12} md={6} className="grid-item">
+            <Grid item xs={12} md={6} className="grid-item">
                 <TextField
                     id="filled-read-only-input"
                     className="inputField"
@@ -30,21 +40,30 @@ function Customer(props) {
                     value={custDetails?.clearBalance ?? ""}
                 />
             </Grid>
-            <Grid xs={12} md={6} className="grid-item">
+            <Grid item xs={12} md={6} className="grid-item">
                 <TextField
+                    className='inputField'
+                    id="outlined-select-currency"
+                    select
+                    label="Currency"
+                    value={currency}
+                    onChange={handleCurrency}
+                    variant="filled"
+                    // helperText="Please select your currency"
+                >   
+                    <MenuItem value="EUR">EUR (<EuroIcon fontSize="inherit"/>)</MenuItem>
+                    <MenuItem value="GBP">GBP (<CurrencyPoundIcon fontSize="inherit"/>)</MenuItem>
+                    <MenuItem value="INR">INR (<CurrencyRupeeIcon fontSize="inherit" />)</MenuItem>
+                    <MenuItem value="JPY">JPY (<CurrencyYenIcon fontSize="inherit" />)</MenuItem>
+                    <MenuItem value="USD">USD (<AttachMoneyIcon fontSize="inherit" />)</MenuItem>
+                </TextField>
+                {/* <TextField
                     className="inputField"
-                    
+
                     id="filled-read-only-input"
                     label="Currency"
                     variant="filled"
-                    value={custDetails?.currency ?? ""} />
-                {/* <TextField select id="filled-basic" label="Currency" variant="filled" >
-                    <MenuItem value="INR" >INR</MenuItem>
-                    <MenuItem value="USD">USD</MenuItem>
-                    <MenuItem value="EUR">EUR</MenuItem>
-                    <MenuItem value="GBP">GBP</MenuItem>
-                    <MenuItem value="JPY">JPY</MenuItem>
-                </TextField> */}
+                    value={custDetails?.currency ?? ""} /> */}
 
             </Grid>
         </>
