3e7189b2a12e05cb958e1ad49f683065d6f6cdfb
ABHISHEK BOMMENA <67004365+AbhishekBommena@users.noreply.github.com>
2022-08-11 13:55:52 +0000

    Add files via upload

--- App.js
+++ App.js
@@ -1,66 +1,21 @@
+import { Container } from '@mui/material';
 import './App.css';
-import Form from './components/Form';
-import ReceiverForm from "./components/ReceiverForm"
-import Button from '@mui/material/Button';
-import NavigateNextIcon from '@mui/icons-material/NavigateNext';
-import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
-import SendIcon from '@mui/icons-material/Send';
-import { useState } from 'react';
-import Container from '@mui/material/Container';
-import Transfer from "./components/Transfer"
-import Grid from "@mui/material/Grid"
+import Transfer from './Transfer';
 
 function App() {
-  const [customer, setCustomer] = useState(null)
-  const [receiver, setReceiver] = useState(null)
-  const [showTransfer, setShowTransfer] = useState(null)
-  const [hideCustReceiver, setHideCustReceiver] = useState(true)
-  function receiveCustomerData(selectedCustomer) {
-    setCustomer(selectedCustomer)
-  }
-  function receiveReceiverData(selectedReceiver) {
-    setReceiver(selectedReceiver)
-  }
-  function handleNext() {
-    if (Object.keys(customer).length !== 0 && Object.keys(receiver).length !== 0) {
-      console.log("cust" + customer.id)
-      console.log("rec" + receiver.id)
-      console.log("welocme")
-      setShowTransfer(true)
-      setHideCustReceiver(false)
-    }
-  }
-  const handleBack = () => {
-    setShowTransfer(false)
-    setHideCustReceiver(true)
-  }
   return (
-    <>
-      <div className="App">
-        <Container maxWidth="sm | md" className='main-container' sx={{ p: "3rem" }}>
-          {hideCustReceiver && <Form receiveCustomerData={receiveCustomerData} />} <br />
-          {hideCustReceiver && <ReceiverForm receiveReceiverData={receiveReceiverData} />}
-          {hideCustReceiver && <Button variant="contained" size="large" endIcon={<NavigateNextIcon />} onClick={handleNext}>
-            Next
-          </Button>}
-          {showTransfer && <Transfer {...customer} />}
-          <Grid container className='grid-content'>
-            <Grid item xs={6} className="grid-item" >
-
-              {showTransfer && <Button variant="contained" size="large" startIcon={<ArrowBackIosNewIcon />} onClick={handleBack}>
-                Back
-              </Button>}
-            </Grid>
-            <Grid item xs={6} className="grid-item" >
-
-              {showTransfer && <Button variant="contained" size="large" endIcon={<SendIcon />}>
-                Transfer
-              </Button>}
-            </Grid>
-            </Grid>
-        </Container>
-      </div>
-    </>
+    <div className='App'>
+      <Container maxWidth="sm | md" className='main-container' 
+      sx={{ 
+        p: "3rem", 
+        bgcolor:"white",
+        borderRadius:"2rem",
+        mt:"5%"
+      }}
+      >
+      <Transfer/>
+      </Container>
+    </div>
   );
 }
 
--- Transfer.js
+++ Transfer.js
@@ -1,62 +1,371 @@
+import './App.css'
+import "./Transfer.css"
+import { useState, useEffect } from 'react';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
-import MenuItem from '@mui/material/MenuItem';
-
-import { useState,useEffect } from 'react';
+import axios from 'axios';
+import { MenuItem } from '@mui/material';
+import CurrencyRupeeIcon from '@mui/icons-material/CurrencyRupee';
+import CurrencyPoundIcon from "@mui/icons-material/CurrencyPound"
+import CurrencyYenIcon from "@mui/icons-material/CurrencyYen"
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney"
+import EuroIcon from "@mui/icons-material/Euro"
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import Typography from '@mui/material/Typography';
 
 function Transfer(props) {
 
-    const [errorHandler, setErrorHandler] = useState(false)
-    const [transferType, setTransferType] = useState('');
-    const [messageCode, setMessageCode] = useState("")
-    const [amountToBeTransfered, setAmountToBeTransfered] = useState("")
-    const [transferFee, setTransferFee] = useState(0);
-    const [afterTransferClearBalance, setAfterTransferClearBalance] = useState(0)
-    const [amountExceedingErr, setAmountExceedingErr] = useState("")
+    // getting date 
+    const dateObject = new Date();
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const date = dateObject.getDate() + " / " + months[dateObject.getMonth()] + " / " + dateObject.getFullYear();
 
-    const handleTransferType = (event) => {
-        setTransferType(event.target.value);
-    };
-    const handleMessageCode = (event) => {
-        setMessageCode(event.target.value);
-    };
+    // customer Variables
+    const [customerErrorHandler, setCustomerErrorHandler] = useState(false)
+    const [customerid, setCustomerId] = useState("");
+    const [custIdNotFound, setCustIdNotFound] = useState(null);
+    const [custDetails, setCustDetails] = useState({});
+    const [clearbalance, setClearBalance] = useState("");
+    const [currencycode, setCurrencyCode] = useState("");
 
-    useEffect( () => {
-        setAmountToBeTransfered("")
-    },[props])
-    useEffect(()=>{
-        if( props.overdraft === "yes"){
-            setTransferFee((amountToBeTransfered*0.25)/100)
-            setAfterTransferClearBalance( parseFloat(props.clearBalance)-(parseFloat(amountToBeTransfered)+parseFloat(transferFee)).toString())
+
+    // receiver Variables
+    const [bic, setBic] = useState("");
+    const [receiverDetails, setReceiverDetails] = useState({});
+    const [receiverErrorHandler, setReceiverErrorHandler] = useState(false)
+    const [receiverNotFound, setReceiverNotFound] = useState(null);
+
+    //transfer variables
+    const [transfertypecode, setTransferTypeCode] = useState("");
+    const [messagecode, setMessageCode] = useState("");
+    const [accountholdernumber, setAccountHolderNumber] = useState("");
+    const [accountholdername, setAccountHolderName] = useState("");
+    const [currencyamount, setCurrencyAmount] = useState("");
+    const [transferfee, setTransferFee] = useState(0);
+    const [inramount, setInrAmount] = useState(0);
+
+    const handleAccountHolderNumber = (e) => {
+        setAccountHolderNumber(e.target.value);
+    }
+
+    const handleAccountHolderName = (e) => {
+        setAccountHolderName(e.target.value);
+    }
+
+    useEffect(() => {
+        if (customerid.length === 5) {
+            axios.get(`http://localhost:8080/Customers/${customerid}`)
+                .then((res) => {
+                    // console.log(res);
+                    setCustDetails(res.data)
+                    setClearBalance(res.data.clearbalance)
+                    setCustIdNotFound(null)
+                    setCustomerErrorHandler(false)
+                })
+                .catch(err => {
+                    setCustDetails({})
+                    setCustomerErrorHandler(true)
+                    setCustIdNotFound("Customer ID Not Found")
+                })
         }
-        else{
-            if( amountToBeTransfered > props.clearBalance){
-                setErrorHandler(true)
-                setAmountExceedingErr("Insufficient Balance")
+        else {
+            if (customerid.length > 0) {
+                setCustomerErrorHandler(true)
+                setCustIdNotFound("Please enter Valid Customer ID")
             }
-            else{
-                setTransferFee((amountToBeTransfered*0.25)/100)
-                setAfterTransferClearBalance( parseFloat(props.clearBalance)-(parseFloat(amountToBeTransfered)+parseFloat(transferFee)))
-                setAmountExceedingErr("")
+            if (customerid.length === 0) {
+                setCustomerErrorHandler(false)
+                setCustIdNotFound("")
             }
-        }        
-    },[amountToBeTransfered])
+            setCustDetails({})
+        }
+    }, [customerid])
+
+    const handleCustomerId = (event) => {
+        event.preventDefault()
+        setCustomerId(event.target.value)
+    }
+    // FOR MAKING RECEIVER FIELDS EMPTY WHEN CUSTOMER ID IS CHANGED
+    // useEffect(() => {
+    //     setReceiverDetails({})
+    //     setBic("")
+    //     setAccountHolderName("")
+    //     setAccountHolderNumber("")
+    // }, [customerid])
+
+    useEffect(() => {
+        const lengthOfBic = bic.length
+        if (lengthOfBic === 11) {
+            axios.get(`http://localhost:8080/receiver/${bic}`)
+                .then((res) => {
+                    setReceiverDetails(res.data)
+                    setReceiverNotFound(null)
+                    setReceiverErrorHandler(false)
+                })
+                .catch(err => {
+                    setReceiverDetails({})
+                    setReceiverErrorHandler(true)
+                    setReceiverNotFound("BIC Not Found ")
+                })
+        }
+        else {
+            if (lengthOfBic > 0) {
+                setReceiverErrorHandler(true)
+                setReceiverNotFound("Please enter valid BIC")
+            }
+            if (lengthOfBic === 0) {
+                setReceiverErrorHandler(false)
+                setReceiverNotFound("")
+            }
+            setReceiverDetails({})
+        }
+    }, [bic])
+
+    const handleWithBic = (event) => {
+        event.preventDefault()
+        setBic(event.target.value)
+    }
+
+    const handleTransferTypeCode = (e) => {
+        setTransferTypeCode(e.target.value);
+    }
+    const handleMessageCode = (e) => {
+        setMessageCode(e.target.value);
+    }
+
+    const handleCurrency = (e) => {
+        setCurrencyCode(e.target.value)
+    }
+
+
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const data = {
+            customerid: customerid, //dbtable:frontend
+            bic: bic,
+            currencycode: currencycode,
+            transferfee: transferfee,
+            receiveraccountholdername: accountholdername,
+            receiveraccountholdernumber: accountholdernumber,
+            transfertypecode: transfertypecode,
+            messagecode: messagecode,
+            currencyamount: currencyamount,
+            transferfees: transferfee,
+            inramount: inramount,
+            // transferdate:setTransferDate(new Date().toLocaleTimeString())
+        };
+        axios.post("http://localhost:8080/api/transactions", data)
+            .then(res => {
+                console.log(res.data)
+
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+
+    const handleCurrencyAmount = (e) => {
+        const currencyamount = e.target.value
+        if (currencyamount > clearbalance) {
+            setCurrencyAmount(currencyamount)
+            setTransferFee("")
+            setInrAmount("")
+        }
+
+        else {
+            setCurrencyAmount(currencyamount)
+            const transferfee = (currencyamount * 0.25) / 100
+            setTransferFee(transferfee)
+            if (currencycode === "USD") {
+                const inramount = currencyamount * 74;
+                const finalAmt = clearbalance - inramount - transferfee
+                setInrAmount(finalAmt)
+            }
+        }
+
+
+    }
+
     return (
         <>
-            <Grid container rowSpacing={3} className='grid-content'>
+            <Grid container rowSpacing={3}>
+                <Grid item xs={12} md={6} className="grid-item">
+                    <Typography variant="h5"
+                        sx={{
+                            color: "#123463",
+                            fontWeight: "bold",
+                            textAlign: "left",
+                            ml: "20%"
+                        }}
+                        gutterBottom component="div">
+                        Payer Account Details
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} md={6} className="grid-item" >
+                    <Typography variant="subtitle2"
+                        sx={{
+                            color: "#123463",
+                            fontWeight: "bold",
+                            textAlign: "left",
+                            ml: "20%"
+                        }}
+                        gutterBottom component="div">
+                        Booking Date : {date}
+                    </Typography>
+                </Grid>
+            </Grid>
+            <Grid container rowSpacing={3}>
+                <Grid item xs={12} md={6} className="grid-item" >
+                    <TextField
+                        required
+                        error={customerErrorHandler}
+                        className="inputField"
+                        helperText={custIdNotFound}
+                        id="filled-basic"
+                        label="Customer ID"
+                        variant="filled"
+                        value={customerid}
+                        onChange={handleCustomerId}
+                    />
+                </Grid>
+                <Grid item xs={12} md={6} className="grid-item">
+                    <TextField id="filled-read-only-input"
+                        InputProps={{
+                            readOnly: true,
+                        }}
+                        label="Account Holder Name"
+                        variant="filled"
+                        className="inputField"
+                        /* CHANGED acctHolderName === accountholdername */
+                        value={custDetails?.acctHolderName ?? ""} />
+                </Grid>
+                <Grid item xs={12} md={6} className="grid-item">
+                    <TextField
+                        id="filled-read-only-input"
+                        className="inputField"
+                        InputProps={{
+                            readOnly: true,
+                        }}
+                        label="Clear Balance"
+                        variant="filled"
+                        /* CHANGED clearBalance === clearbalance */
+                        value={custDetails?.clearBalance ?? ""}
+                    />
+                </Grid>
+                <Grid item xs={12} md={6} className="grid-item">
+                    <TextField
+                        className='inputField'
+                        id="outlined-select-currency"
+                        select
+                        label="Currency"
+                        value={currencycode}
+                        onChange={handleCurrency}
+                        variant="filled"
+                    // helperText="Please select your currency"
+                    >
+                        <MenuItem value="EUR">EUR (<EuroIcon fontSize="inherit" />)</MenuItem>
+                        <MenuItem value="GBP">GBP (<CurrencyPoundIcon fontSize="inherit" />)</MenuItem>
+                        <MenuItem value="INR">INR (<CurrencyRupeeIcon fontSize="inherit" />)</MenuItem>
+                        <MenuItem value="JPY">JPY (<CurrencyYenIcon fontSize="inherit" />)</MenuItem>
+                        <MenuItem value="USD">USD (<AttachMoneyIcon fontSize="inherit" />)</MenuItem>
+                    </TextField>
+                </Grid>
+            </Grid>
+            <Grid container rowSpacing={3} sx={{ mt: "8px" }}>
+                <Grid item xs={12} md={6} className="grid-item">
+                    <Typography variant="h5"
+                        sx={{
+                            color: "#123463",
+                            fontWeight: "bold",
+                            textAlign: "left",
+                            ml: "20%"
+                        }}
+                        gutterBottom component="div">
+                        Beneficiary Account Details
+                    </Typography>
+                </Grid>
+            </Grid>
+            <Grid container rowSpacing={3}>
+                <Grid item xs={12} md={6} className="grid-item" >
+                    <TextField
+                        required
+                        error={receiverErrorHandler}
+                        className="inputField"
+                        helperText={receiverNotFound}
+                        id="filled-basic"
+                        label="BIC"
+                        variant="filled"
+                        value={bic}
+                        onChange={handleWithBic}
+                    />
+                </Grid>
+                <Grid item xs={12} md={6} className="grid-item">
+                    <TextField id="filled-read-only-input"
+                        InputProps={{
+                            readOnly: true,
+                        }}
+                        label="Bank Name"
+                        variant="filled"
+                        className="inputField"
+                        /* CHANGED instituteName === bankname */
+                        value={receiverDetails?.instituteName ?? ""}
+                    />
+                </Grid>
+                <Grid item xs={12} md={6} className="grid-item">
+                    <TextField
+                        required
+                        id="filled-read-only-input"
+                        className="inputField"
+                        label="Account Holder Name"
+                        variant="filled"
+                        value={accountholdername}
+                        onChange={handleAccountHolderName}
+                    />
+                </Grid>
+                <Grid item xs={12} md={6} className="grid-item">
+                    <TextField
+                        required
+                        className="inputField"
+                        id="filled-read-only-input"
+                        label="Account Holder Number"
+                        variant="filled"
+                        value={accountholdernumber}
+                        onChange={handleAccountHolderNumber}
+                    />
+                </Grid>
+            </Grid>
+            <Grid container rowSpacing={3} sx= {{ mt:"8px"}}>
+                <Grid item xs={12} md={6} className="grid-item">
+                    <Typography variant="h5"
+                        sx={{
+                            color: "#123463",
+                            fontWeight: "bold",
+                            textAlign: "left",
+                            ml: "20%"
+                        }}
+                        gutterBottom component="div">
+                            Transfer Details
+                    </Typography>
+                </Grid>
+            </Grid>
+            <Grid container rowSpacing={3}>
                 <Grid item xs={12} md={6} className="grid-item">
                     <TextField
                         className='inputField'
                         id="outlined-select-currency"
                         select
                         label="Transfer Type"
-                        value={transferType}
-                        onChange={handleTransferType}
+                        value={transfertypecode}
+                        onChange={handleTransferTypeCode}
                         variant="filled"
                     // helperText="Please select your currency"
                     >
-                        <MenuItem value="customer">Customer</MenuItem>
-                        <MenuItem value="Bank">Bank</MenuItem>
+                        <MenuItem value="Customer Type">Customer</MenuItem>
+                        <MenuItem value="Bank Type">Bank</MenuItem>
                     </TextField>
 
                 </Grid>
@@ -66,12 +375,12 @@ function Transfer(props) {
                         id="outlined-select-currency"
                         select
                         label="Message Code"
-                        value={messageCode}
+                        value={messagecode}
                         onChange={handleMessageCode}
                         variant="filled"
                     // helperText="Please select your currency"
                     >
-                        <MenuItem value="customer">CHQB</MenuItem>
+                        <MenuItem value="CHQB">CHQB</MenuItem>
                         <MenuItem value="CORT">CORT</MenuItem>
                         <MenuItem value="HOLD">HOLD</MenuItem>
                         <MenuItem value="INTC">INTC</MenuItem>
@@ -87,36 +396,50 @@ function Transfer(props) {
                         label="Amount"
                         variant="filled"
                         className="inputField"
-                        error={errorHandler}
-                        helperText={amountExceedingErr}
-                        value={amountToBeTransfered}
-                        onChange={(event) => { setAmountToBeTransfered(event.target.value) }}
+                        // error={errorHandler}
+                        // helperText={amountExceedingErr}
+                        value={currencyamount}
+                        onChange={handleCurrencyAmount}
                     />
                 </Grid>
                 <Grid item xs={12} md={6} className="grid-item">
                     <TextField
                         InputProps={{
-                            readOnly:true
+                            readOnly: true
                         }}
                         label="Transfer Fee"
                         variant="filled"
                         className="inputField"
-                        value={transferFee}
+                        value={transferfee}
                     />
                 </Grid>
                 <Grid item xs={12} md={6} className="grid-item">
                     <TextField
                         InputProps={{
-                            readOnly:true
+                            readOnly: true
                         }}
                         label="Clear Balance"
                         variant="filled"
                         className="inputField"
-                        value={afterTransferClearBalance}
+                        value={inramount}
                     />
+                </Grid>
+                <Grid item xs={12} md={6} className="grid-item">
+                    <Button
+                        variant="contained"
+                        size="medium"
+                        endIcon={<SendIcon />}
+                        sx={{
+                            bgcolor: "#123463"
+                        }}
+                        onClick={handleSubmit}
+                    >
+                        Transfer
+                    </Button>
                 </Grid>
             </Grid>
         </>
     )
+
 }
 export default Transfer;
